9e465890085409476cc89a5d9542de63c90e31cc
snowdot <co.pascu@gmail.com>
2021-01-29 17:09:20 +0200

    added countdown

---
+++ script.js
@@ -0,0 +1,26 @@
+const countdown = document.getElementById('countdown');
+const days = document.getElementById('days');
+const hours = document.getElementById('hours');
+const minutes = document.getElementById('minutes');
+const seconds = document.getElementById('seconds');
+const date = new Date('Jan 29, 2021 22:00:00').getTime();
+
+const x = setInterval(function () {
+    const now = new Date().getTime();
+    const remaining = date - now;
+
+    const d = Math.floor(remaining / (1000 * 60 * 60 * 24));
+    const h = Math.floor((remaining % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const m = Math.floor((remaining % (1000 * 60 * 60)) / (1000 * 60));
+    const s = Math.floor((remaining % (1000 * 60)) / 1000);
+
+    days.innerHTML = d;
+    hours.innerHTML = h;
+    minutes.innerHTML = m;
+    seconds.innerHTML = s;
+
+    if (remaining < 0) {
+        clearInterval(x);
+        countdown.innerHTML = 'HODL!';
+    }
+}, 1000);
